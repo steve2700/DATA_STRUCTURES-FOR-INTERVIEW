173882855e54091583dd8688c74a43be10a4c11a
steve2700 <stevnyaruwa@gmail.com>
2023-03-06 14:06:00 +0200

    atlanta

---
+++ coding_challenges/maths.py
@@ -0,0 +1,17 @@
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    # the sum , difference , product
+    
+    sum_ab = a + b
+    diff_ab = a - b
+    product_ab = a * b
+    
+    print(sum_ab)
+    print(diff_ab)
+    print(product_ab)
+    
+    
+    
