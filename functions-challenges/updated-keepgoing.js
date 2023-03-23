6073a8c00293643b916a02b7c9ba161dbfd1a9b7
steve2700 <stevnyaruwa@gmail.com>
2023-03-23 13:10:35 +0200

    im loving javascript

---
+++ functions-challenges/updated-keepgoing.js
@@ -0,0 +1,8 @@
+function NevergiveUp(name) {
+    console.log(`Dont give up ${name}! you can do this!`);
+    for (let x = 0; x < 1000; x++) {
+        console.log(`Keep going ${name}! you are making progress!`);
+        console.log(`Great Job ${name}! you never gave up!`);
+    }
+}
+NevergiveUp('Stewart');
