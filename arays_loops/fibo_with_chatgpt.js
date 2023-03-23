51c1a42db88aadbbc40b5b8f738a690cdb45168c
steve2700 <stevnyaruwa@gmail.com>
2023-03-23 21:22:21 +0200

    this code u will enjoy

---
+++ arays_loops/fibo_with_chatgpt.js
@@ -0,0 +1,16 @@
+let sum = 0;
+
+for (let i = 1; i <= 10; i++ ) {
+    sum += i;
+    console.log(sum);
+}
+
+
+// this one we are using a while loop;
+// let sum = 0;
+// let i = 1;
+while(i <= 10) {
+    sum += i;
+    console.log(sum);
+    i++;
+}
