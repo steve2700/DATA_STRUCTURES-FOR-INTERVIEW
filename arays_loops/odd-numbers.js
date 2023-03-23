9b0be41d194c88ad88c9d77ed1e4c08a409d71e7
steve2700 <stevnyaruwa@gmail.com>
2023-03-23 16:26:04 +0200

    odd numbers are so good

---
+++ arays_loops/odd-numbers.js
@@ -0,0 +1,15 @@
+for (let x = 1; x <= 100; x += 2) {
+    console.log(x);
+
+}
+// all odd numbers
+
+// using while loop 
+let x = 1;
+while(x >= 50); {
+    if (x % 2 == 1);{
+        console.log(x);
+    }
+    x++;
+}
+
