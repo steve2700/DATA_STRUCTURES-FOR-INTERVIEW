bd070f49484a836c00af36b7487b3ef3002ad5e6
steve2700 <stevnyaruwa@gmail.com>
2023-03-23 14:29:29 +0200

    yes sir

---
+++ arays_loops/0-sum.js
@@ -0,0 +1,8 @@
+//Write a JavaScript program to find the sum of all elements in an array.
+let numbers = [1, 3, 5, 7, 8, 9, 10];
+let sum = 0;
+for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+
+}
+console.log("The sum of this: " , sum)
