a12258cdfc8fa180b771b8675dd5e8dd57e8cc75
steve2700 <stevnyaruwa@gmail.com>
2023-03-23 14:56:31 +0200

    bigger and smaller numbers

---
+++ arays_loops/max-min.js
@@ -0,0 +1,8 @@
+let numbers = [10, 20, 40, 50, 40, 20,80];
+// find the bigger number
+let bigger_number = Math.max(...numbers);
+console.log('The biggest number is:',bigger_number)
+
+// find the smallest number 
+let smaller_number = Math.min(...numbers);
+console.log('The smallest number is:',smaller_number);
