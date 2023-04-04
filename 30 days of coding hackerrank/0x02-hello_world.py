e3267121b0970892d30090b77a6e8d1ec0301799
Stewart Nyaruwata <112034196+steve2700@users.noreply.github.com>
2023-04-05 01:21:33 +0200

    update

---
+++ 30 days of coding hackerrank/0x02-hello_world.py
@@ -0,0 +1,14 @@
+#good lucky buddy):
+#To complete this challenge, you must save a line of input from stdin to a variable, print Hello, World. on a single line, and finally print the value of your variable on a second line.
+#Print Hello, World. on the first line, and the contents of inputstring  on the second line.
+#and dont forget hackerrank is fun!
+#input is already there
+inputstring = input()
+#print hello world on the first line
+print('Hello, World')
+#and the contents on the second line
+print(inputstring)
+
+#day 1 to the end! python skills
+
+
