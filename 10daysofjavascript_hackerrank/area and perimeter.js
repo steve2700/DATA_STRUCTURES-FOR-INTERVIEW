d43521949823ef8f66b7cedbd395d15de81781c2
Stewart Nyaruwata <112034196+steve2700@users.noreply.github.com>
2023-04-07 01:33:07 +0200

    Create area and perimeter.js
    JAVASCRIPT IS AMAZING

---
+++ 10daysofjavascript_hackerrank/area and perimeter.js
@@ -0,0 +1,55 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   Calculate the area of a rectangle.
+*
+*   length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the rectangle's area.
+**/
+function getArea(length, width) {
+    let area;
+    //calculation to geth the area
+    area = (length * width);
+    return area;
+}
+
+/**
+*   Calculate the perimeter of a rectangle.
+*	
+*	length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the perimeter of a rectangle.
+**/
+function getPerimeter(length, width) {
+    let perimeter;
+    // calculation to get the permiter
+    perimeter = (length + width + length + width);
+    
+    return perimeter;
+}
+
