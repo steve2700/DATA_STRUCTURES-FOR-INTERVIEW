24698e33f525732d3439803688a87291f5f13288
Stewart Nyaruwata <112034196+steve2700@users.noreply.github.com>
2023-04-13 02:14:54 +0200

    Create vending_machines.py

---
+++ 30 days of coding hackerrank/vending_machines.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+class VendingMachine:
+    # Implement the VendingMachine here
+    def __init__(self, num_items, item_coins):
+        self.num_items = num_items
+        self.item_coins = item_coins
+    def buy(self, num_items, item_coins):
+        if num_items > self.num_items:
+            raise ValueError("Not enough items in the machine")
+        total_cost = num_items * self.item_coins
+        if num_coins < total_cost:
+            raise ValueError("Not enough coins")
+            
+        self.num_items -= num_items
+        return num_coins - total_cost
+            
+    pass
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    num_items, item_coins = map(int, input().split())
+    machine = VendingMachine(num_items, item_coins)
+
+    n = int(input())
+    for _ in range(n):
+        num_items, num_coins = map(int, input().split())
+        try:
+            change = machine.buy(num_items, num_coins)
+            fptr.write(str(change) + "\n")
+        except ValueError as e:
+            fptr.write(str(e) + "\n")
+
+
+    fptr.close()
