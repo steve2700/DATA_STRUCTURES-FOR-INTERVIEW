610fd5298434b52f47740de2cd6636cd8171c451
Stewart Nyaruwata <112034196+steve2700@users.noreply.github.com>
2023-05-07 15:12:01 +0200

    Create day3 -Closures.js

---
+++ leetcode_Interview/day3 -Closures.js
@@ -0,0 +1,31 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
+ var createCounter = function(init) {
+  let currentCount = init;
+  return {
+    increment: function() {
+       currentCount += 1;
+       return currentCount;
+    },
+    decrement: function() {
+       currentCount -= 1;
+       return currentCount;
+    },
+    reset: function() {
+       currentCount = init;
+       return currentCount;
+    },
+  }
+};
