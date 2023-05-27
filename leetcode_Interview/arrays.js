8500b2947cfdf04478b702fabb6af2988f202bff
Stewart Nyaruwata <112034196+steve2700@users.noreply.github.com>
2023-05-27 21:54:39 +0200

    Create arrays.js

---
+++ leetcode_Interview/arrays.js
@@ -0,0 +1,14 @@
+if (!Array.prototype.findLast) {
+    Array.prototype.findLast = function(predicate) {
+        for (let i = this.length - 1; i >= 0; i--) {
+            if (predicate(this[i], i, this)) {
+                return this[i];
+            }
+        }
+        return undefined;
+    };
+}
+
+Array.prototype.last = function() {
+  return this.findLast(() => true) ?? -1;
+}
